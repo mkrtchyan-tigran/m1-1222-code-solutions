28116de9c1fc9ca51b3d4ed7724dda316095a684
Tigran Mkrtchyan <tigranmkrtchyan59@gmail.com>
2022-12-23 04:29:52 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,41 @@
+var width = 10;
+var height = 20;
+
+var area = width * height;
+console.log('value of area:', area);
+console.log('type of area:', typeof area);
+
+var bill = 60;
+var payment = 100;
+
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('type of change:', typeof change);
+
+var quizzes = 3;
+var midterm = 4;
+var final = 5;
+var grade = ((quizzes + midterm + final) / 3);
+
+console.log('value of grade:', grade);
+console.log('type of grade:', typeof grade);
+
+var firstName = 'Tigran';
+var lastName = 'Mkrtchyan';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+var pH = 4;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('type of is Acidic:', typeof isAcidic);
+
+var headCount = 200;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = fullName += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('type of motto:', typeof motto);
